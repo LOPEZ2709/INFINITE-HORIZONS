243aadd830cdcebaf102c7164fe3048180dcc842
LOPEZby2006 <162337066+LOPEZ2709@users.noreply.github.com>
2024-08-23 17:07:17 -0500

    Add files via upload

---
+++ CODIGO/pagina/Infinite_Horizons/Login/Inicio de sesion-20240517T234632Z-001/Inicio de sesion/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+session_start(); // Inicia una nueva sesión o reanuda una existente
+
+// Verificar si el usuario ha iniciado sesión
+if (!isset($_SESSION['email'])) {
+    header("Location: index2.html"); // Redirige al formulario de inicio de sesión si no está autenticado
+    exit();
+}
+
+echo "<h1>Bienvenido, " . $_SESSION['email'] . "!</h1>";
+echo "<p>Has iniciado sesión correctamente.</p>";
