8e2dad8a480e444fdfef9b5a7ed451f8d493469c
Kristin-Vanessa-Restrepo-Moreno <167988290+Kristin-Vanessa-Restrepo-Moreno@users.noreply.github.com>
2025-03-25 07:49:10 -0500

    Delete CODIGO directory

--- CODIGO/dashboard/dashboard.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();  // Iniciar la sesión
-session_destroy();  // Destruir la sesión
-header("Location: ../index.html");  // Redirigir a la página de inicio o login
-exit();  // Asegurar que el script termina aquí
