6494308db363a0012e4c7e01499096b584ecdba7
Kristin-Vanessa-Restrepo-Moreno <167988290+Kristin-Vanessa-Restrepo-Moreno@users.noreply.github.com>
2025-03-25 07:50:37 -0500

    Delete destinix-react directory

--- destinix-react/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Inicia o reanuda la sesión
-session_unset(); // Elimina todas las variables de sesión
-session_destroy(); // Destruye la sesión activa
-
-// Redirige al formulario de inicio de sesión
-header("Location: /login");
-exit();
-?>
