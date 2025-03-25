3c780058c9679a1fe51a9e136d91e89e354e3fed
Kristin-Vanessa-Restrepo-Moreno <167988290+Kristin-Vanessa-Restrepo-Moreno@users.noreply.github.com>
2025-03-25 07:37:57 -0500

    Delete CRUD-ADMINISTRADOR directory

--- CRUD-ADMINISTRADOR/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $conexion = new PDO('mysql:host=localhost;dbname=destinix;charset=utf8', 'root', '');
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error en la conexión: " . $e->getMessage());
-}
-?>
--- CRUD-ADMINISTRADOR/dash-admin/hoteles/mensaje.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET['status']) && $_GET['status'] == 'success') {
-    echo "Actualización correcta.";
-} else {
-    echo "Hubo un problema al actualizar los datos.";
-}
-?>
