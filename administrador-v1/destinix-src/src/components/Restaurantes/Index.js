ddc188d70050f81a8e17ddbca2c46cc9fae21e1d
lexnardos <166026532+lexnardos@users.noreply.github.com>
2025-04-22 20:23:46 -0500

    Delete administrador-v1/destinix-src/src directory

--- administrador-v1/destinix-src/src/components/Restaurantes/Index.js
+++
@@ -1,9 +0,0 @@
-export { default as RestaurantesManager } from './RestaurantesManager';
-export { default as RestauranteForm } from './RestaurantesForm';
-export { default as RestauranteCard } from './RestaurantesCard';
-
-export {
-  RestaurantesManager,
-  RestauranteForm,
-  RestauranteCard
-};
