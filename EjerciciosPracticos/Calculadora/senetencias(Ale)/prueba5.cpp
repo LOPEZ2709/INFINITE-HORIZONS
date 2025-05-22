c26b6ab8bdd074de1debc5cd372c914ff2ef829f
Ale <166027833+Alejandra-Florez@users.noreply.github.com>
2025-05-21 19:37:52 -0500

    Add files via upload

---
+++ EjerciciosPracticos/Calculadora/senetencias(Ale)/prueba5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero = 10;
+    if (numero % 2 == 0) {
+        cout << "El número es par." << endl;
+    } else {
+        cout << "El número es impar." << endl;
+    }
+    return 0;
+}
