0d66523cde9c7d0b173d213829dc6ffd1264749e
Ale <166027833+Alejandra-Florez@users.noreply.github.com>
2025-05-21 19:37:24 -0500

    Add files via upload

---
+++ EjerciciosPracticos/Calculadora/senetencias(Ale)/prueba4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int contador = 0;
+    while (contador < 18) {
+        cout << "Contador: " << contador << endl;
+        contador++;
+    }
+    return 0;
+}
