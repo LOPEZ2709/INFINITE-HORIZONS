d1782d31d3c5b00936b2bb13bd91f40cfd95d0aa
Ale <166027833+Alejandra-Florez@users.noreply.github.com>
2025-05-21 19:35:53 -0500

    Add files via upload

---
+++ EjerciciosPracticos/Calculadora/senetencias(Ale)/prueba2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  
+using namespace std; /
+
+int main() {
+    int edad;
+    cout << "¿Cuántos anos tienes? ";
+    cin >> edad;
+    cout << "Tienes " << edad << " años." << endl;
+    return 0;
+}
